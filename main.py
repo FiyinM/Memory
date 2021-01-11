8b98c8c48625d1d1923bdcb28b8ed4c804e2882e
FiyinM <77219099+FiyinM@users.noreply.github.com>
2021-01-11 09:24:44 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-"""
-def repeat_directions():
-    while True:
-        def b_pressed():
-            if input.button_is_pressed(Button.B):
-                basic.show_arrow(ArrowNames.NORTH, 500)
-        def shake():
-            if input.is_gesture(Gesture.Shake):
-                basic.show_arrow(ArrowNames.SOUTH, 500)
-        input.on_button_pressed(Button.B, b_pressed)
-        input.on_gesture(Gesture.Shake, shake)
-"""
-
-
 def show_directions():
     directions = [ArrowNames.NORTH, ArrowNames.SOUTH]
-    choose_direction_1 = directions[randint(0,1)]
-    choose_direction_2 = directions[randint(0,1)]
-    choose_direction_3 = directions[randint(0,1)]
-    choose_direction_4 = directions[randint(0,1)]
-    basic.show_arrow(choose_direction_1, 500)
+    random_directions = []
+    user_directions = [0]
+    num_1 = randint(0,1)
+    num_2 = randint(0,1)
+    num_3 = randint(0,1)
+    num_4 = randint(0,1)
+    choose_direction_1 = num_1
+    random_directions.append(choose_direction_1)
+    choose_direction_2 = num_2
+    random_directions.append(choose_direction_2)
+    choose_direction_3 = num_3
+    random_directions.append(choose_direction_3)
+    choose_direction_4 = num_4
+    random_directions.append(choose_direction_4)
+    basic.show_arrow(directions[choose_direction_1], 500)
     basic.show_string("", 200)
-    basic.show_arrow(choose_direction_2, 500)
+    basic.show_arrow(directions[choose_direction_2], 500)
     basic.show_string("", 200)
-    basic.show_arrow(choose_direction_3, 500)
+    basic.show_arrow(directions[choose_direction_3], 500)
     basic.show_string("", 200)
-    basic.show_arrow(choose_direction_4, 500)
+    basic.show_arrow(directions[choose_direction_4], 500)
     basic.show_string("", 200)
-    while True:
+    user_directions.remove(user_directions[0])
+    while user_directions != random_directions:
         if input.button_is_pressed(Button.A):
             basic.show_arrow(ArrowNames.NORTH, 500)
+            user_directions.append(0)
         if input.button_is_pressed(Button.B):
-                basic.show_arrow(ArrowNames.SOUTH, 500)
-
-
-    
-
-    
-#input.on_button_pressed(Button.B, repeat_directions)
-
+            basic.show_arrow(ArrowNames.SOUTH, 500)
+            user_directions.append(1)
+    basic.show_icon(IconNames.HAPPY)
+            
 input.on_button_pressed(Button.A, show_directions)
 
 
--- main.ts
+++ main.ts
@@ -1,39 +1,39 @@
-/** 
-def repeat_directions():
-    while True:
-        def b_pressed():
-            if input.button_is_pressed(Button.B):
-                basic.show_arrow(ArrowNames.NORTH, 500)
-        def shake():
-            if input.is_gesture(Gesture.Shake):
-                basic.show_arrow(ArrowNames.SOUTH, 500)
-        input.on_button_pressed(Button.B, b_pressed)
-        input.on_gesture(Gesture.Shake, shake)
-
- */
-// input.on_button_pressed(Button.B, repeat_directions)
 input.onButtonPressed(Button.A, function show_directions() {
     let directions = [ArrowNames.North, ArrowNames.South]
-    let choose_direction_1 = directions[randint(0, 1)]
-    let choose_direction_2 = directions[randint(0, 1)]
-    let choose_direction_3 = directions[randint(0, 1)]
-    let choose_direction_4 = directions[randint(0, 1)]
-    basic.showArrow(choose_direction_1, 500)
+    let random_directions = []
+    let user_directions = [0]
+    let num_1 = randint(0, 1)
+    let num_2 = randint(0, 1)
+    let num_3 = randint(0, 1)
+    let num_4 = randint(0, 1)
+    let choose_direction_1 = num_1
+    random_directions.push(choose_direction_1)
+    let choose_direction_2 = num_2
+    random_directions.push(choose_direction_2)
+    let choose_direction_3 = num_3
+    random_directions.push(choose_direction_3)
+    let choose_direction_4 = num_4
+    random_directions.push(choose_direction_4)
+    basic.showArrow(directions[choose_direction_1], 500)
     basic.showString("", 200)
-    basic.showArrow(choose_direction_2, 500)
+    basic.showArrow(directions[choose_direction_2], 500)
     basic.showString("", 200)
-    basic.showArrow(choose_direction_3, 500)
+    basic.showArrow(directions[choose_direction_3], 500)
     basic.showString("", 200)
-    basic.showArrow(choose_direction_4, 500)
+    basic.showArrow(directions[choose_direction_4], 500)
     basic.showString("", 200)
-    while (true) {
+    user_directions.removeElement(user_directions[0])
+    while (user_directions != random_directions) {
         if (input.buttonIsPressed(Button.A)) {
             basic.showArrow(ArrowNames.North, 500)
+            user_directions.push(0)
         }
         
         if (input.buttonIsPressed(Button.B)) {
             basic.showArrow(ArrowNames.South, 500)
+            user_directions.push(1)
         }
         
     }
+    basic.showIcon(IconNames.Happy)
 })
